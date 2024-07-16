3dd17a3d8e84db9dc91b6f69078f7bb917663057
Pablo-r-stack <pablor.velasco.suarez@gmail.com>
2024-07-16 01:17:03 -0300

    se arregla bug visual en formulario de carga

--- src/pages/NewVideo.jsx
+++ src/pages/NewVideo.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Form from '../components/Form/Form'
+import Banner from '../components/Banner/Banner'
 
 export default function NewVideo(props) {
   const { categories, fetchVideos } = props
   return (
-    <div className='container flex flex-col gap-5 items-center p-5 h-screen'>
+    <>
+      <Banner />
+      <div className='w-full flex flex-col gap-5 items-center justify-center p-3 '>
         <h2 className='text-3xl font-bold my-5'>Cargar Nuevo video</h2>
         <Form categories={categories} fetchVideos={fetchVideos} />
-    </div>
+      </div>
+    </>
   )
 }
